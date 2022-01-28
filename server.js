a630ed3c37cd0b056d8af573c37e7cd20604fb0a
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-28 10:38:56 +0600

    video chat app initiated

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const {v4: uuidv4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
